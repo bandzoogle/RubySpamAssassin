9c57e165169e00a1786b0cdef2df4b3c286c5ded
Colin Mitchell <muffinista@gmail.com>
2017-08-24 16:38:45 -0400

    Fix parsing issue
    Extra letters were being squeezed, so 'message' became 'mesage'

--- spec/RubySpamAssassin/report_parser_spec.rb
+++ spec/RubySpamAssassin/report_parser_spec.rb
@@ -4,11 +4,16 @@ describe "ReportParser" do
   it "should parse the report text into an informative hash" do
     spam = File.read('spec/data/spam_test1.txt')
     result = RubySpamAssassin::ReportParser.parse(spam)
-    result.length.equal?(6)
+
+    expect(result.length).to eql(6)
 
     # Check contents of first one to make sure text/points are formatted correctly
-    result[0][:pts].equal?(0.5)
-    result[0][:rule].equal?('DATE_IN_PAST_24_48')
-    result[0][:text].equal?('Date: is 24 to 48 hours before Received: date')
+    expect(result[0][:pts]).to eql(0.5)
+    expect(result[0][:rule]).to eql('DATE_IN_PAST_24_48')
+    expect(result[0][:text]).to eql('Date: is 24 to 48 hours before Received: date')
+
+    expect(result[1][:pts]).to eql(0.0)
+    expect(result[1][:rule]).to eql('HTML_MESSAGE')
+    expect(result[1][:text]).to eql('BODY: HTML included in message')
   end
 end
